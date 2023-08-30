ef842e6aac43a6a741dd3be766ce96d518214d23
김지아 <wltkd0515@naver.com>
2023-08-30 20:26:57 +0900

    [add]이슈 페이지 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Workspace from "./page/Workspace";
-import Kanban from "./page/Kanban";
-import Issue from "./page/issue/Issue";
-import IssueDetail from "./page/issue/IssueDetail";
-import NotFound from "./page/NotFound";
-import HeaderWithToken from "./layout/HeaderWithToken";
-import SideBar from "./layout/SideBar";
-import WorkspaceLayout from "./layout/WorkspaceLayout";
-
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Workspace from './page/Workspace';
+import Kanban from './page/Kanban';
+import Issue from './page/issue/Issue';
+import IssueDetail from './page/issue/IssueDetail';
+import NotFound from './page/NotFound';
+import HeaderWithToken from './layout/HeaderWithToken';
+import SideBar from './layout/SideBar';
+import WorkspaceLayout from './layout/WorkspaceLayout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import * as ReactBootstrap from 'react-bootstrap';
 function App() {
   // const location = window.location;
   // const isMoingSpace = location.hostname.startsWith("moingspace");
 
-  const hasToken = localStorage.getItem("Authorization") !== null;
+  const hasToken = localStorage.getItem('Authorization') !== null;
 
   // "moingspace" 서브도메인에 접속하면 다른 URL로 리다이렉트
   // if (isMoingSpace) {
--- src/page/issue/Issue.tsx
+++ src/page/issue/Issue.tsx
@@ -1,7 +1,61 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Nav from 'react-bootstrap/Nav';
+import Table from 'react-bootstrap/Table';
 
 export default function Issue() {
-  return <IssueLayoutMain>이슈</IssueLayoutMain>;
+  return (
+    <IssueLayoutMain>
+      <Box>
+        <Nav fill variant="tabs" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                {Array.from({ length: 7 }).map((_, index) => (
+                  <th key={index}>Table heading</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                {Array.from({ length: 7 }).map((_, index) => (
+                  <td key={index}>Table cell {index}</td>
+                ))}
+              </tr>
+              <tr>
+                <td>2</td>
+                {Array.from({ length: 7 }).map((_, index) => (
+                  <td key={index}>Table cell {index}</td>
+                ))}
+              </tr>
+              <tr>
+                <td>3</td>
+                {Array.from({ length: 7 }).map((_, index) => (
+                  <td key={index}>Table cell {index}</td>
+                ))}
+              </tr>
+            </tbody>
+          </Table>
+        </Nav>
+      </Box>
+    </IssueLayoutMain>
+  );
 }
 
 const IssueLayoutMain = styled.main`
@@ -10,3 +64,25 @@ const IssueLayoutMain = styled.main`
   justify-content: center;
   background: pink;
 `;
+const Box = styled.div`
+  display: flex;
+  width: auto;
+  background: rebeccapurple;
+`;
+
+<Nav fill variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>;
