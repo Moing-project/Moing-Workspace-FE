3c20d3d507336c37c3cbbdbe2e437ffc489e97fa
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-09-04 01:01:35 +0900

    [test] 토큰 없을 시 로그인 페이지로 리다이렉팅 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import "./App.css";
 import Workspace from "./page/Workspace";
 import Kanban from "./page/Kanban";
 import Issue from "./page/issue/Issue";
-import IssueDetail from "./page/issue/IssueDetail";
 import NotFound from "./page/NotFound";
-import HeaderWithToken from "./layout/HeaderWithToken";
-import SideBar from "./layout/SideBar";
 import WorkspaceLayout from "./layout/WorkspaceLayout";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -18,22 +15,24 @@ function App() {
 
   // "moingspace" 서브도메인에 접속하면 다른 URL로 리다이렉트
   if (isMoingSpace) {
-    return (
-      <>
-        <Routes>
-          <Route element={<WorkspaceLayout />}>
-            <Route path="/" element={<Workspace />} />
-            <Route path="/:id" element={<Workspace />} />
-            <Route path="/:id/Kanban" element={<Kanban />} />
-            <Route path="/:id/issue" element={<Issue />}>
-              {/* <Route path="/:id/detail/:issueId" element={<IssueDetail />} /> */}
+    if (hasToken) {
+      return (
+        <>
+          <Routes>
+            <Route element={<WorkspaceLayout />}>
+              <Route path="/" element={<Workspace />} />
+              <Route path="/:id" element={<Workspace />} />
+              <Route path="/:id/Kanban" element={<Kanban />} />
+              <Route path="/:id/issue" element={<Issue />}>
+                {/* <Route path="/:id/detail/:issueId" element={<IssueDetail />} /> */}
+              </Route>
+              {/* 상단에 위치하는 라우트들의 규칙 중 일치하는 라우트가 없다면 아래가 화면에 나타남 */}
+              <Route path="*" element={<NotFound />}></Route>
             </Route>
-            {/* 상단에 위치하는 라우트들의 규칙 중 일치하는 라우트가 없다면 아래가 화면에 나타남 */}
-            <Route path="*" element={<NotFound />}></Route>
-          </Route>
-        </Routes>
-      </>
-    );
+          </Routes>
+        </>
+      );
+    } else window.location.href = "moingtool.com/login";
   } else return null;
 }
 
